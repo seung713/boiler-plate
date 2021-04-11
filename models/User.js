8e4470b83b64f1a5d797717e9dafd75539234301
sengeun <rlatmddms98@naver.com>
2021-04-11 22:44:23 +0900

    hash pwd

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 1
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +14,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        maxlength: 100
     },
     lastname: {
         type: String,
@@ -28,6 +30,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this
+    if(user.isModified('password')) {
+        //저장 전 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
     }
 })
 
